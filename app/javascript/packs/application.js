273d8e7c394bb0909c55a1e647e3d1f2de6f8f5b
gowsik-ragunath <v.gowsik@gmail.com>
2020-03-14 23:58:53 +0530

    Changes in UI

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("stylesheets/application.scss")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -20,7 +21,5 @@ require("trix")
 require("@rails/actiontext")
 
 import "controllers"
-
-import "bootstrap";
 
 import "@fortawesome/fontawesome-free/js/all";
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: "thoughts#index"
+  
   resources :thoughts
   post "embeds", to: "embeds#create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
